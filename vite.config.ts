7ccaa0055bb423b5e406faa3af9023d49375b02d
Jakob Norlin <jakob.stahl91@gmail.com>
2024-01-06 17:21:44 +0100

    Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
-import { defineConfig } from "@solidjs/start/config";
+import { defineConfig } from "@solidjs/start/config"
 
-export default defineConfig({});
+export default defineConfig({
+	start: {
+		server: {
+			preset: "cloudflare-pages",
+
+			// We will need to enable CF Pages node compatiblity
+			// https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+			rollupConfig: {
+				external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+			},
+		},
+	},
+})
