96366cd07ac8b4f7054eb99d316fee60c2065875
Jakob Norlin <jakob.stahl91@gmail.com>
2024-01-06 17:27:37 +0100

    abc === 123

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,25 @@
 // @refresh reload
-import { createSignal } from "solid-js";
-import "./app.css";
+import { createSignal } from "solid-js"
+import "./app.css"
+
+if (process.env.abc !== "123") throw new Error("abc is not 123")
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
+	const [count, setCount] = createSignal(0)
 
-  return (
-    <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
+	return (
+		<main>
+			<h1>Hello world!</h1>
+			<button class="increment" onClick={() => setCount(count() + 1)}>
+				Clicks: {count()}
+			</button>
+			<p>
+				Visit{" "}
+				<a href="https://start.solidjs.com" target="_blank">
+					start.solidjs.com
+				</a>{" "}
+				to learn how to build SolidStart apps.
+			</p>
+		</main>
+	)
 }
